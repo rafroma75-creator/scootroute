c21753138283de91ad4c6f0a605ae34cb55df157
rafroma75-creator <rafroma75@gmail.com>
2026-07-15 18:05:03 +0200

    Create road.js

---
+++ road.js
@@ -0,0 +1,11 @@
+const scooterRules = {
+    avoidMotorways: true,
+    avoidTrunkRoads: true
+};
+
+const forbiddenRoadTypes = [
+    "motorway",
+    "motorway_link",
+    "trunk",
+    "trunk_link"
+];
